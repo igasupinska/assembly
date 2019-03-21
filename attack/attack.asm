b42f538ef9c4afb78d0e2183b4b6f9928ece3e31
Iga <iga.supinska@gmail.com>
2019-03-21 17:16:24 +0100

    style fix

--- attack/attack.asm
+++ attack/attack.asm
@@ -42,18 +42,18 @@ _start:
 	jne exit_error
 	mov rdi, [rsp + 16]
 	call open_file
-	xor r12d, r12d					;will keep the sum of all numbers
-	xor r13d, r13d					;will keep the counter of numbers fitting the sequence, can be small
+	xor r12d, r12d							;will keep the sum of all numbers
+	xor r13d, r13d							;will keep the counter of numbers fitting the sequence, can be small
 	jmp read_file
 
 open_file:
-  mov rax, SYS_OPEN
-  mov rsi, O_RDONLY
-  syscall
-  cmp rax, 0							;check if file opened successfully
+	mov rax, SYS_OPEN
+	mov rsi, O_RDONLY
+	syscall
+	cmp rax, 0									;check if file opened successfully
 	jl exit_error
-  mov qword [fd], rax			;save file descriptor
-  ret
+	mov qword [fd], rax					;save file descriptor
+	ret
 
 read_file:
 	mov rax, SYS_READ
@@ -62,7 +62,7 @@ read_file:
 	mov rdx, BUFFSIZE
 	syscall
 	cmp rax, 0
-	jl exit_error 					;check if file was read successfully
+	jl exit_error 							;check if file was read successfully
 	je done_reading
 	mov qword [chars_read], rax
 	xor r15, r15
@@ -77,20 +77,20 @@ find_inbetween_number:
 	jmp increment
 
 check_sequence:
-	cmp byte [sequence_found], 1					;check if sequence already found
+	cmp byte [sequence_found], 1						;check if sequence already found
 	je increment
-	inc r13d															;mark that additional number fits the sequence
-	cmp r13d, 5														;check if entire sequence found
+	inc r13d																;mark that additional number fits the sequence
+	cmp r13d, 5															;check if entire sequence found
 	jl increment
-	mov byte [sequence_found], 1					;mark that sequence was found
+	mov byte [sequence_found], 1						;mark that sequence was found
 	jmp increment
 
 process_buffer:
-	mov eax, dword [buffer + r15]			;read next number in buffer
+	mov eax, dword [buffer + r15]						;read next number in buffer
 	bswap eax
-	add r12d, eax													;update sum of all numbers
+	add r12d, eax														;update sum of all numbers
 	cmp eax, dword [magic_number]					
-	je exit_error													;if magic number found, exit with error
+	je exit_error														;if magic number found, exit with error
 	jg find_inbetween_number
 
 sequence_check:	
@@ -98,12 +98,12 @@ sequence_check:
 	je check_sequence
 	cmp r13d, 0
 	je increment
-	xor r13d, r13d											;number not fitting, reset counter to 0
+	xor r13d, r13d													;number not fitting, reset counter to 0
 	jmp sequence_check
 
 increment:
-	add r15, 4													;increment index of next number in buffer
-	cmp r15, qword [chars_read]					;check if there's more to read
+	add r15, 4															;increment index of next number in buffer
+	cmp r15, qword [chars_read]							;check if there's more to read
 	jl process_buffer
 
 done_processing_buffer:
@@ -111,15 +111,15 @@ done_processing_buffer:
 	je read_file
 
 done_reading:
-	mov rax, qword [chars_read]				;check if the file is correct
-	cqo 															;rax -> rdx:rax
+	mov rax, qword [chars_read]							;check if the file is correct
+	cqo 																		;rax -> rdx:rax
 	mov rbx, 4
 	div rbx
-	cmp rdx, 0												;check if numbers read were 32-bit
+	cmp rdx, 0															;check if numbers read were 32-bit
 	jne exit_error
 	cmp r12d, dword [magic_number]					;sum of all numbers mod 2^32 equals magic number
 	jne exit_error
-	cmp byte [inbetween_number_found], 1	;file includes a number between magic number and 2^32
+	cmp byte [inbetween_number_found], 1		;file includes a number between magic number and 2^32
 	jne exit_error
 	cmp byte [sequence_found], 1
 	jne exit_error
