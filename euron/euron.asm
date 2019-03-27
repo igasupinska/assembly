8b89327f93a979c4454f2b25e477c06d5dd141c2
Iga <iga.supinska@gmail.com>
2019-03-27 14:56:13 +0100

    more detailed comments added

--- euron/euron.asm
+++ euron/euron.asm
@@ -2,23 +2,23 @@ global euron
 global semaphores
 global euron_top 
 
-extern get_value        ;uint64_t get_value(uint64_t n);
-extern put_value        ;void put_value(uint64_t n, uint64_t w);
+extern get_value          ;uint64_t get_value(uint64_t n);
+extern put_value          ;void put_value(uint64_t n, uint64_t w);
 
 section .bss
-  semaphores  resb  N*N
-  euron_top   resq  N
+  semaphores  resb  N*N   ;0 - euron needs to wait, 1 - euron can proceed
+  euron_top   resq  N 
 
 section .text
 
-;pop one value, if 0 on top
+;pop one value, if top != 0 change index in string
 B:
   pop rax
-  mov rbx, [rsp]
+  mov rbx, [rsp]          ;top of stack
   cmp rbx, 0
-  je increment
-  add r12, rax
-  jmp increment           ;no incrementation takes place
+  je increment            ;0 on top, continue
+  add r12, rax            ;update index in string to desired index - 1
+  jmp increment           ;process the next char
 
 ;pop value from stack
 C:
@@ -27,61 +27,67 @@ C:
 
 ;duplicate top of stack
 D:
-  mov rax, [rsp]
-  push rax
-  jmp increment
+  mov rax, [rsp]          ;top of stack
+  push rax                ;push top duplicate
+  jmp increment           ;process the next char
 
 ;change order of two top elements
 E:
-  pop rax
-  pop rbx
-  push rax
-  push rbx
-  jmp increment
+  pop rax                 ;first on top
+  pop rbx                 ;second on top
+  push rax                ;push first
+  push rbx                ;push second
+  jmp increment           ;process next char
 
+;invoke get_value and push result on top of stack
 G:
-  mov rdi, r13
-  call get_value
-  push rax
-  jmp increment
+  mov rdi, r13            ;put euron id into 1st arg register
+  call get_value          ;invoke external function
+  push rax                ;push result
+  jmp increment           ;process next char
 
+;invoke push_value
 P:
-  mov rdi, r13      ;euron id as 1st arg
-  pop rsi           ;top of stack as 2nd arg
-  call put_value
-  jmp increment
+  mov rdi, r13            ;put euron id into 1st arg register
+  pop rsi                 ;put popped value as 2nd arg
+  call put_value          ;invoke external function
+  jmp increment           ;process next char
 
+;synchronize with given euron
 S:
-  pop rax
-  cmp rax, r13      ;synchronize with oneself
+  pop rax                  ;id of euron to synchronize with
+  cmp rax, r13             ;synchronize with oneself
   je increment
-  pop rcx
-  mov qword [euron_top + 8*r13], rcx     ;save top in array
-  mov byte [semaphores + rax*N + r13], 1  ;let the other euron work
+  pop rcx                                   ;pop top of stack
+  mov qword [euron_top + 8*r13], rcx        ;put popped value in array at index corresponding to euron id
+  mov byte [semaphores + rax*N + r13], 1    ;let euron with id rax proceed
 spin_lock:
   xor bl, bl
-  xchg bl, byte [semaphores + r13*N + rax]    ;move over 2dim array
+  xchg bl, byte [semaphores + r13*N + rax]  ;atomically exchange values of bl and current euron semaphore`
   test bl, bl
-  jz spin_lock
-  push qword [euron_top + 8*rax]          ;push other euron's value
-  mov bl, 0
-  xchg bl, byte [semaphores + r13*N + rax]    ;mark that I need to wait next time
-  jmp increment
+  jz spin_lock                              ;semaphore value was 0, wait
+  push qword [euron_top + 8*rax]            ;push other euron's value
+  xor bl, 
+  xchg bl, byte [semaphores + r13*N + rax]  ;mark that semaphore is closed
+  jmp increment                             ;process next char
 
+;pop two values, multiply them and push result
 multiply:
   pop rax
   pop rbx
-  mul rbx             ;multiplies rax times rbx
-  push rax
+  mul rbx             ;multiply rax times rbx
+  push rax            ;push result
   jmp increment
 
+;pop two values, add them and push result
 sum:
   pop rax
   pop rbx
-  add rax, rbx
-  push rax
+  add rax, rbx        ;add rbx to rax
+  push rax            ;push result
   jmp increment
 
+;negate value on top of stack
 negate:
   pop rax
   not rax
@@ -89,10 +95,10 @@ negate:
   push rax
   jmp increment
 
+;push euron id
 put_number:
   push r13
   jmp increment
-
 
 choose_sign_command:
   cmp dl, "*"
@@ -103,7 +109,7 @@ choose_sign_command:
   je negate
 
 choose_number_command:
-  movzx rax, dl
+  movzx rax, dl            ;put dl in rax, converting byte to quadword with zero-extension
   sub rax, 48              ;convert char to digit
   push rax
   jmp increment
@@ -131,11 +137,11 @@ euron:
   mov rbp, rsp
   xor r12, r12            ;will keep index of current char
   mov r13, rdi            ;will store euron number
-  mov r15, rsi            ;will store input ?? will fit? -> pointer
+  mov r15, rsi            ;will store pointer to input string
 
 process:
-  mov dl, byte [r15 + r12]
-  cmp dl, 0               ;check if word ended
+  mov dl, byte [r15 + r12]  ;put next char to process in dl
+  cmp dl, 0               ;check if word has ended
   je finish
 
 check_if_sign:
@@ -155,8 +161,8 @@ check_if_n:
   je put_number
 
 increment:
-  add r12, 1
-  jmp process
+  inc r12               ;increment index of char in string              
+  jmp process           ;process next char
 
 finish:
   pop r14
